c735acab06f0146a72938c362e658db5740bb2cf
QueenAyana <amberrose9167@gmail.com git config --global user.name QueenAyanagit config --global user.email amberrose9167@gmail.com>
2018-09-13 20:17:14 -0700

    final push

--- assets/js/giff-script.js
+++ assets/js/giff-script.js
@@ -1,7 +1,8 @@
-var cartoonTopics = ["dexters laboratory", "powerpuff girls", "ren and stimpy", "ed edd and eddy", "johnny bravo", "catdog", "kim posible", "my life as a teenage robot", "tom and jerry", "looney toons"]
+var cartoonTopics = ["dexters laboratory", "powerpuff girls", "ren and stimpy", "ed edd and eddy", "johnny bravo", "catdog", "kim possible", "my life as a teenage robot", "tom and jerry", "looney toons"]
 
 console.log(cartoonTopics)
 var toonName = "";
+var dataNum = 0
 buttonMkr();
 function buttonMkr() {
     cartoonTopics.forEach(function (cartoonName) {
@@ -14,16 +15,17 @@ function buttonMkr() {
         newButton.on("click", function () {
             var itemValue = cartoonName;
             console.log(itemValue);
-            toonName = itemValue.replace(/\s+/g, '');
+            toonName = itemValue//.replace(/\s+/g, '');
             console.log(toonName);
+            dataNum ++
         })
         $("#gifDiv").append(newButton)
     })
 }
 
-$("button").on("click", function () {
+$("body").on("click", "#toonBtn", function(){
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=V0z7PqjeqSOoPLF5G0O2bj7xJM2k7Hm5&q" + toonName;
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=V0z7PqjeqSOoPLF5G0O2bj7xJM2k7Hm5&q=" + toonName
 
     console.log(queryURL)
     $.ajax({
@@ -32,19 +34,33 @@ $("button").on("click", function () {
     })
         .then(function (response) {
 
-            var imageUrl = response.data.image_original_url;
+            var imageURL = response.data[dataNum].images.original_still.url;
+            var gifURL = response.data[dataNum].images.original.url;
             // Creating and storing an image tag
             var toonImage = $("<img>");
-
+            
+            console.log(imageURL)
+            console.log(response.data)
             // Setting the toonImage src attribute to imageUrl
-            toonImage.attr("src", imageUrl);
+            toonImage.attr("src", imageURL);
             toonImage.attr("alt", toonName + " image");
+            console.log(toonImage[0].src)
             // append to the dom
-            $("gifPocket").append(toonImage)
+            $("#gifPocket").append(toonImage)
+            
+            toonImage.hover(function () {
+                toonImage.attr("src", gifURL);
+                toonImage.attr("alt", toonName + " gif");
+            },null)
         })
+
 })
 $("#funnBtn").on("click", function () {
-    alert("this is submited")
+    var addToon = $("#addCartoon").val().trim()
+    cartoonTopics.push(addToon)
+    $("#gifDiv").empty()
+    buttonMkr();
+    console.log(cartoonTopics)
 });
 //build queryurl 
 //make ajax call
